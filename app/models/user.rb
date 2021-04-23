6315cc4da4361a2f7d4afd9c8716be33a6c1131c
koutaro02 <k.u89yakyuu@icloud.com>
2021-04-23 16:29:00 +0900

    userモデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :position
+
+  with_options presence: true do
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は、全角（漢字・ひらがな・カタカナ）で入力してください' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は、全角（漢字・ひらがな・カタカナ）で入力してください'}
+    validates :phone_number, format: { with: /\A\d{11}\z/, message: 'は、(-)ハイフン無し、または11桁以内の数字で入力してください'}
+    validates :company_name, format: { with: /\A[A-Zぁ-んァ-ン一-龥]+\z/, message: 'は、全角（漢字・ひらがな・カタカナ）、または英字(大文字)で入力してください'}
+  end
+
+  validates :position_id, numericality: { other_than: 0, message: "can't be blank"}
 end
