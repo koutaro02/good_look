2f84cee83e543afe061c1c11f040a2dfed36312b
koutaro02 <k.u89yakyuu@icloud.com>
2021-04-23 17:04:23 +0900

    mapモデルのバリデーション

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,2 +1,15 @@
 class Map < ApplicationRecord
+  has_many :users
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :spot_name
+    validates :address
+    validates :latitude
+    validates :longitude
+
+  end
+
+  geocoded_by :address
+  before_validation :geocode
 end
